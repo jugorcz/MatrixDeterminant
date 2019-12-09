602e62cbae6eca8f8a87f13fb6d91a1182b4942d
Justyna Gorczyca <just.gorcz@gmail.com>
2019-12-09 20:46:21 +0100

    det calculate

--- main.py
+++ main.py
@@ -4,66 +4,152 @@ from PlotStuff import Plot
 from PlotStuff import Scene
 import random
 import math
+import numpy
 
 
-def det_way1(a, b, c):
-    return a[0] * b[1] + a[1] * c[0] + b[0] * c[1] - b[1] * c[0] - c[1] * a[0] - a[1] * b[0]
-
-
-def det_way2(a, b, c):
+def det_mode1(a, b, c):
     a11 = a[0] - c[0]
     a12 = a[1] - c[1]
     a21 = b[0] - c[0]
     a22 = b[1] - c[1]
-    return a11 * a22 - a21 * a12
+    det = a11 * a22 - a21 * a12
+    return det, False
 
 
-def split_list(points_list, point_a, point_b):
+def det_mode2(a, b, c):
+    row1 = [a[0], a[1], 1]
+    row2 = [b[0], b[1], 1]
+    row3 = [c[0], c[1], 1]
+    array = numpy.array([row1, row2, row3])
+    det = numpy.linalg.det(array)
+    return det, False
+
+
+def det_mode3(a, b, c):
+    a11 = a[0] - c[0]
+    a12 = a[1] - c[1]
+    a21 = b[0] - c[0]
+    a22 = b[1] - c[1]
+    det = a11 * a22 - a21 * a12
+    return det, False
+
+
+def det_mode4(a, b, c):
+    row1 = [a[0] - c[0], a[1] - c[1]]
+    row2 = [b[0] - c[0], b[1] - c[1]]
+    array = numpy.array([row1, row2])
+    det = numpy.linalg.det(array)
+    return det, False
+
+
+def det_mode12(a, b, c):
+    det = a[0] * b[1] + a[1] * c[0] + b[0] * c[1] - b[1] * c[0] - c[1] * a[0] - a[1] * b[0]
+
+    # use numpy function
+    row1 = [a[0], a[1], 1]
+    row2 = [b[0], b[1], 1]
+    row3 = [c[0], c[1], 1]
+    array = numpy.array([row1, row2, row3])
+    det2 = numpy.linalg.det(array)
+    # print("Det way1: " + str(det) + " -> " + str(det2))
+
+    problem = True
+    if (det > 0 and det2 > 0) or (det < 0 and det2 < 0):
+        problem = False
+    return det2, problem
+
+
+def det_mode34(a, b, c):
+    a11 = a[0] - c[0]
+    a12 = a[1] - c[1]
+    a21 = b[0] - c[0]
+    a22 = b[1] - c[1]
+    det = a11 * a22 - a21 * a12
+
+    # use numpy function
+    row1 = [a[0] - c[0], a[1] - c[1]]
+    row2 = [b[0] - c[0], b[1] - c[1]]
+    array = numpy.array([row1, row2])
+    det2 = numpy.linalg.det(array)
+    # print("Det way2: " + str(det) + " -> " + str(det2))
+
+    problem = True
+    if (det > 0 and det2 > 0) or (det < 0 and det2 < 0):
+        problem = False
+    return det, problem
+
+
+def split_list(points_list, mode):
+    point_a = (-1.0, 0.0)
+    point_b = (1.0, 0.1)
     left_points_list = []
     right_points_list = []
     center_points_list = []
+    problem_list = []
     for point in points_list:
-        det = det_way2(point_a, point_b, point)
-        if det < 0:
+
+        if mode == 1:
+            det, problem = det_mode1(point_a, point_b, point)
+        elif mode == 2:
+            det, problem = det_mode2(point_a, point_b, point)
+        elif mode == 3:
+            det, problem = det_mode3(point_a, point_b, point)
+        elif mode == 4:
+            det, problem = det_mode4(point_a, point_b, point)
+        elif mode == 12:
+            det, problem = det_mode12(point_a, point_b, point)
+        elif mode == 34:
+            det, problem = det_mode34(point_a, point_b, point)
+        else:
+            det, problem = det_mode1(point_a, point_b, point)
+
+        if problem:
+            problem_list.append(point)
+        elif det < 0:
             left_points_list.append(point)
         elif det > 0:
             right_points_list.append(point)
         else:
             center_points_list.append(point)
-        return left_points_list, right_points_list, center_points_list
+
+    print("\nLeft points: " + str(len(left_points_list)))
+    print("Right points: " + str(len(right_points_list)))
+    print("Center points: " + str(len(center_points_list)))
+    print("Problem points: " + str(len(problem_list)))
+    return left_points_list, right_points_list, center_points_list, problem_list
 
 
-def display_splitted_list(points_list, point_a, point_b):
-    left_points_list, right_points_list, center_points_list = split_list(points_list, point_a, point_b)
-    left_points_collection = PointsCollection(left_points_list, color='blue', marker="1")
-    right_points_collection = PointsCollection(right_points_list, color='green', marker="1")
-    center_points_collection = PointsCollection(center_points_list, color='red', marker="*")
-
-    scene = Scene([right_points_collection, left_points_collection, center_points_collection], [])
-    split_plot = Plot([scene])
-    split_plot.draw()
+def get_point(x):
+    a = 0.05
+    b = 0.05
+    y = a * x + b
+    return x, y
 
 
-def main():
+def display_splitted_list(points_list, min_x, max_x, plot, det_mode):
+    left_points_list, right_points_list, center_points_list, problem_list = split_list(points_list, det_mode)
+    left_points_collection = PointsCollection(left_points_list, color='blue', marker=".")
+    right_points_collection = PointsCollection(right_points_list, color='green', marker=".")
+    center_points_collection = PointsCollection(center_points_list, color='red', marker=".")
+    problem_points_collection = PointsCollection(problem_list, color='yellow', marker=".")
+    lines_collection = LinesCollection([[get_point(min_x), get_point(max_x)]], color='yellow')
+
+    scene = Scene([right_points_collection, left_points_collection, center_points_collection, problem_points_collection], [])
+    plot.add_scene(scene)
+
+
+def generate_data():
     points_list_a = []
-    for i in range(10 ** 2):
+    for i in range(10 ** 4):
         x = random.randint(-1000, 1000)
         y = random.randint(-1000, 1000)
         points_list_a.append((int(x), int(y)))
 
-    points_collection_a = PointsCollection(points_list_a, color='blue', marker="1")
-    plot_a = Plot([points_collection_a])
-    # plot_a.draw()
-
     points_list_b = []
-    for i in range(10 ** 2):
-        x = random.randint(-10 ** 2, 10 ** 2)
-        y = random.randint(-10 ** 2, 10 ** 2)
+    for i in range(10 ** 4):
+        x = random.randint(-10 ** 14, 10 ** 14)
+        y = random.randint(-10 ** 14, 10 ** 14)
         points_list_b.append((int(x), int(y)))
-
-    points_collection_b = PointsCollection(points_list_b, color='red', marker="1")
-    plot_b = Plot([points_collection_b])
-    # plot_b.draw()
 
     points_list_c = []
     center_x = 0
@@ -75,38 +161,43 @@ def main():
         alpha = 2 * math.pi * random.random()
         x = radius * math.cos(alpha) + center_x
         y = radius * math.sin(alpha) + center_y
-        points_list_c.append((int(x), int(y)))
-
-    points_from_line = []
-
-    points_collection_c = PointsCollection(points_list_c, color='green', marker="1")
-    line_collection = LinesCollection([points_from_line])
-    plot_c = Plot([points_collection_c])
-    # plot_c.draw()
+        points_list_c.append((float(x), float(y)))
 
     points_list_d = []
-    a = 0.05
-    b = 0.05
-    i = 1000
-    while i > 0:
+    for i in range(250):
         x = random.randint(-1000, 1000)
-        y = a * x + b
-        if y in range(-1000, 1000):
-            i -= 1
-            points_list_d.append((int(x), int(y)))
+        points_list_d.append(get_point(x))
 
-    points_collection_d = PointsCollection(points_list_d, color='green', marker="*")
-    plot_d = Plot([points_collection_d])
-    # plot_d.draw()
+    return points_list_a, points_list_b, points_list_c, points_list_d
 
-    point_a = (-1.0, 0.0)
-    point_b = (1.0, 0.1)
-    line = LinesCollection([[point_a, point_b]])
-    scene = Scene([], [line])
-    line_plot = Plot([scene])
-    line_plot.draw()
 
-    display_splitted_list(points_list_d, point_a, point_b)
+def display(points_list, lines_list):
+    points_collection_list = []
+    lines_collection_list = []
+    if points_list:
+        points_collection = PointsCollection(points_list,  marker=".")
+        points_collection_list.append(points_collection)
+    if lines_list:
+        lines_collection = LinesCollection(lines_list)
+        lines_collection_list.append(lines_collection)
+
+    scene = Scene(points_collection_list, lines_collection_list)
+    plot = Plot([scene])
+    plot.draw()
+
+
+def main():
+    list_a, list_b, list_c, list_d = generate_data()
+    display(list_d, [])
+    plot = Plot([])
+    # display_splitted_list(list_a, -1000, 1000, plot, False)
+    # display_splitted_list(list_b, -10 ** 14, 10 ** 14, plot, False)
+    # display_splitted_list(list_c, -100, 100, plot, False)
+    display_splitted_list(list_d, -1000, 1000, plot, 1)
+    display_splitted_list(list_d, -1000, 1000, plot, 2)
+    display_splitted_list(list_d, -1000, 1000, plot, 3)
+    display_splitted_list(list_d, -1000, 1000, plot, 4)
+    plot.draw()
 
 
 if __name__ == '__main__':
